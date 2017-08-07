a854bea2dffa486fcbd245e946c81b02f0b00280
K <x00.x7f@gmail.com>
2017-08-07 23:18:42 +0900

    Project init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::Format - It's new $module
+
+# SYNOPSIS
+
+    use App::Format;
+
+# DESCRIPTION
+
+App::Format is ...
+
+# LICENSE
+
+Copyright (C) K.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+K <x00.x7f@gmail.com>
---
+++ lib/App/Format.pm
@@ -0,0 +1,39 @@
+package App::Format;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Format - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::Format;
+
+=head1 DESCRIPTION
+
+App::Format is ...
+
+=head1 LICENSE
+
+Copyright (C) K.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+K E<lt>x00.x7f@gmail.comE<gt>
+
+=cut
+
